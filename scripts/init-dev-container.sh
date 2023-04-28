959ca7b21c8ed88d677435543d7ace338a3f0c68
furuse-km <takeru.furuse@konicaminolta.com>
2023-04-28 18:28:47 +0900

    add git-completion

--- scripts/init-dev-container.sh
+++ scripts/init-dev-container.sh
@@ -12,13 +12,15 @@ check_string="Git prompt settings"
 if grep -q "$check_string" ~/.bashrc; then
     echo "Git prompt settings already exist in .bashrc"
 else
-    # git promptをダウンロード
+    # git-prompt
     git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
-
-    # .bashrcにプロンプト設定を追加https://github.com/kmmsjp/gofracta
     echo -e "\n\n# Git prompt settings" >> ~/.bashrc
     echo "export GIT_PROMPT_ONLY_IN_REPO=1" >> ~/.bashrc
     echo "source ~/.bash-git-prompt/gitprompt.sh" >> ~/.bashrc
+
+    # git-completion
+    curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+    echo "source ~/.git-completion.bash" >> ~/.bashrc
 
     # 設定を反映
     source ~/.bashrc
